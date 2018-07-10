7ffd5486124349e8d8741abdfece5b84273a2def
Gabor Torok <gtorok@babbel.com>
2018-07-10 18:22:22 +0200

    Add boilerplate for backend projects for Neos
    Includes:
    - Express
    - Mocha
    - Eslint

---
+++ test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./server');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('GET /', function() {
+
+	it('Hello world is displayed', function(done) {
+		chai.request(app).get('/').end((err, res) => {
+			res.should.have.status(200);
+			res.body.should.be.equal('Hello World');
+			done();
+		});
+	});
+
+});
