ad19d07d15e8b41a4bb33b386e7ae41944cf215b
Gabor Torok <gtorok@babbel.com>
2018-07-16 13:07:26 +0200

    Remove client side specific details from server related ESlint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,19 +1,12 @@
 module.exports = {
     "env": {
-        "browser": true,
         "es6": true,
         "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": 2018
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
         "indent": [
             "error",
